0d7a3a71876d55aca653e0f74fda0c1eb4b899a7
Dmitry Yakovlev <Dimon1998daf@mail.ru>
2022-04-23 15:13:57 +0600

    Lesson5

---
+++ TimeSheetsL5/TimeSheets/DAL/Interfaces/IContract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeSheets.Controllers.Models;
+
+namespace TimeSheets.DAL.Interfaces
+{
+    interface IContract
+    {
+        List<Contract> GetAllContaract();
+        Contract SearchContract(int id);
+
+        void AddContract(Contract contracted);
+
+        void UpdateContract(int searchUpdate);
+
+        void DeleteContract(int DeleteValue);
+        Contract SearchContractByName(string name);
+
+    }
+}
